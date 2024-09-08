a79d92d3c1c396f3f6bf9d6d10e5665975a169a6
kozhinpm <kozhinpm24@yandex.ru>
2024-09-08 08:45:20 +0700

    Home work 6-2 ThreadPoolTask

--- src/main/java/course/concurrency/m6_streams/ThreadPoolTask.java
+++ src/main/java/course/concurrency/m6_streams/ThreadPoolTask.java
@@ -1,16 +1,43 @@
 package course.concurrency.m6_streams;
 
-import java.util.concurrent.*;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolTask {
 
-    // Task #1
-    public ThreadPoolExecutor getLifoExecutor() {
-        return null;
+  private class LIFOQueue<E> extends LinkedBlockingDeque<E> {
+    @Override
+    public E take() throws InterruptedException {
+      return super.takeLast();
     }
 
-    // Task #2
-    public ThreadPoolExecutor getRejectExecutor() {
-        return null;
+    @Override
+    public E poll() {
+      return super.pollLast();
     }
+
+    @Override
+    public E peek() {
+      return super.peekLast();
+    }
+  }
+
+  // Task #1
+  public ThreadPoolExecutor getLifoExecutor() {
+    return new ThreadPoolExecutor(
+        0, Integer.MAX_VALUE, 0, TimeUnit.MILLISECONDS, new LIFOQueue<>());
+  }
+
+  // Task #2
+  public ThreadPoolExecutor getRejectExecutor() {
+    return new ThreadPoolExecutor(
+        8,
+        8,
+        0,
+        TimeUnit.MILLISECONDS,
+        new SynchronousQueue<>(),
+        new ThreadPoolExecutor.DiscardPolicy());
+  }
 }
